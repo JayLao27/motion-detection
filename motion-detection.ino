977c5726a3ed40cb30a1b240fd7beeb90a858914
Jay Lao <cjaylao447@gmail.com>
2025-12-05 23:32:08 +0800

    led pin modified

--- motion-detection.ino
+++ motion-detection.ino
@@ -1,46 +1,46 @@
-#define SECOND 1000UL
-#define MINUTE (60UL * SECOND)
+  #define SECOND 1000UL
+  #define MINUTE (60UL * SECOND)
 
-int ledPin = 7;               
-int pirPin = 6;                
+  int ledPin = 13;               
+  int pirPin = 6;                
 
-int lastPirVal = LOW;          
-int pirVal;                   
+  int lastPirVal = LOW;          
+  int pirVal;                   
 
-unsigned long myTime;        
-char printBuffer[128];         
- 
-void setup() {
-  pinMode(ledPin, OUTPUT);    
-  pinMode(pirPin, INPUT);     
- 
-  Serial.begin(9600);
-}
- 
-void loop() {
-  pirVal = digitalRead(pirPin); 
+  unsigned long myTime;        
+  char printBuffer[128];         
+  
+  void setup() {
+    pinMode(ledPin, OUTPUT);    
+    pinMode(pirPin, INPUT);     
+  
+    Serial.begin(9600);
+  }
+  
+  void loop() {
+    pirVal = digitalRead(pirPin); 
 
-  if (pirVal == HIGH) {   
-    digitalWrite(ledPin, HIGH); 
+    if (pirVal == HIGH) {   
+      digitalWrite(ledPin, HIGH); 
 
-    if (lastPirVal == LOW) { 
-      myTime = millis();
-      sprintf(printBuffer, "%lu min %lu sec: Motion detected!",
-              myTime / MINUTE,
-              (myTime % MINUTE) / SECOND);
-      Serial.println(printBuffer);
-      lastPirVal = HIGH;
-    }
-  } else {
-    digitalWrite(ledPin, LOW); 
+      if (lastPirVal == LOW) { 
+        myTime = millis();
+        sprintf(printBuffer, "%lu min %lu sec: Motion detected!",
+                myTime / MINUTE,
+                (myTime % MINUTE) / SECOND);
+        Serial.println(printBuffer);
+        lastPirVal = HIGH;
+      }
+    } else {
+      digitalWrite(ledPin, LOW); 
 
-    if (lastPirVal == HIGH) {
-      myTime = millis();
-      sprintf(printBuffer, "%lu min %lu sec: Motion ended!",
-              myTime / MINUTE,
-              (myTime % MINUTE) / SECOND);
-      Serial.println(printBuffer);
-      lastPirVal = LOW;
+      if (lastPirVal == HIGH) {
+        myTime = millis();
+        sprintf(printBuffer, "%lu min %lu sec: Motion ended!",
+                myTime / MINUTE,
+                (myTime % MINUTE) / SECOND);
+        Serial.println(printBuffer);
+        lastPirVal = LOW;
+      }
     }
   }
-}
